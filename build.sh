674267e070c1e90a07b24ab1fe430866dd17f2b8
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2014-11-26 12:39:00 +0100

    new configuration option

--- build.sh
+++ build.sh
@@ -5,19 +5,19 @@
 # Configuration 
 #--------------------------------------------------------------------------------------------------------------------------------
 
-BOARD="cubietruck"						# cubietruck, cubieboard2, bananapi, cubox-i, bananapi, lime, lime2
-BRANCH="default"						# default=3.4.x, mainline=next
-VERSION="Cubietruck Debian 2.9 $RELEASE"               		# just name
-SOURCE_COMPILE="yes"                               		# yes / no
-KERNEL_CONFIGURE="yes"						# do you want to change my default configuration
-DEST_LANG="en_US.UTF-8"                         	 	# sl_SI.UTF-8, en_US.UTF-8
-TZDATA="Europe/Ljubljana"                         		# Timezone
-DEST=$(pwd)/output                      		      	# Destination
-ROOTPWD="1234"                               		  	# Must be changed @first login
-HOST="cubie"						 	# Hostname
-USEALLCORES="no"						# Use all CPU cores for compiling
-SDSIZE="1000"							# SD image size in MB
-FBTFT="no"							# Small TFT support, https://github.com/notro/fbtft
+BOARD="cubietruck"					# cubietruck, cubieboard2, bananapi, cubox-i, bananapi, lime, lime2
+BRANCH="default"					# default=3.4.x, mainline=next
+VERSION="Cubietruck Debian 2.9 $RELEASE"               	# just name
+SOURCE_COMPILE="yes"                               	# yes / no
+KERNEL_CONFIGURE="yes"					# do you want to change my default configuration
+DEST_LANG="en_US.UTF-8"                         	# sl_SI.UTF-8, en_US.UTF-8
+TZDATA="Europe/Ljubljana"                         	# Timezone
+DEST=$(pwd)/output                      		# Destination
+ROOTPWD="1234"                               		# Must be changed @first login
+HOST="cubie"						# Hostname
+USEALLCORES="no"					# Use all CPU cores for compiling
+SDSIZE="1000"						# SD image size in MB
+FBTFT="no"						# Small TFT support, https://github.com/notro/fbtft
 
 #--------------------------------------------------------------------------------------------------------------------------------
 # superuser have to do this
